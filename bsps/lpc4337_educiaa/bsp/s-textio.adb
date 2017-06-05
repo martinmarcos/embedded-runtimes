62e4becfdde0ec2fcd5539886c1310ef284c8e85
Martin Marcos <mmarcos.arg@gmail.com>
2017-06-05 11:38:26 -0300

    Fix s-textio.adb for lpc4337_educiaa

--- bsps/lpc4337_educiaa/bsp/s-textio.adb
+++ bsps/lpc4337_educiaa/bsp/s-textio.adb
@@ -28,16 +28,16 @@
 
 with Interfaces; use Interfaces;
 
-with Interfaces.Bit_Types;   use Interfaces.Bit_Types;
-with Interfaces.STM32.RCC;   use Interfaces.STM32.RCC;
-with Interfaces.STM32.GPIO;  use Interfaces.STM32.GPIO;
-with Interfaces.STM32.USART; use Interfaces.STM32.USART;
-with System.STM32;           use System.STM32;
+with Interfaces.LPC4337.SCU;   use Interfaces.LPC4337.SCU;
+with Interfaces.LPC4337.CCU1;  use Interfaces.LPC4337.CCU1;
+with Interfaces.LPC4337.CCU2;  use Interfaces.LPC4337.CCU2;
+with Interfaces.LPC4337.USART; use Interfaces.LPC4337.USART;
+
 with System.BB.Parameters;
 
 package body System.Text_IO is
 
-   Baudrate : constant := 115_200;
+   --  Baudrate : constant := 115_200;
    --  Bitrate to use
 
    ----------------
@@ -45,35 +45,72 @@ package body System.Text_IO is
    ----------------
 
    procedure Initialize is
-      use System.BB.Parameters;
-
-      APB_Clock    : constant Positive := Positive (STM32.System_Clocks.PCLK2);
-      Int_Divider  : constant Positive := (25 * APB_Clock) / (4 * Baudrate);
-      Frac_Divider : constant Natural := Int_Divider rem 100;
-
    begin
       Initialized := True;
 
-      RCC_Periph.APB2ENR.USART1EN := 1;
-      RCC_Periph.AHB1ENR.GPIOBEN  := 1;
+      SCU_Periph.SFSP7 (1).EPD := Disable_Pull_Down;
+      SCU_Periph.SFSP7 (1).EPUN := Disable_Pull_Up;
+      SCU_Periph.SFSP7 (1).MODE := Function_6;
 
-      GPIOB_Periph.MODER.Arr     (6 .. 7) := (Mode_AF,     Mode_AF);
-      GPIOB_Periph.OSPEEDR.Arr   (6 .. 7) := (Speed_50MHz, Speed_50MHz);
-      GPIOB_Periph.OTYPER.OT.Arr (6 .. 7) := (Push_Pull,   Push_Pull);
-      GPIOB_Periph.PUPDR.Arr     (6 .. 7) := (Pull_Up,     Pull_Up);
-      GPIOB_Periph.AFRL.Arr      (6 .. 7) := (AF_USART1,   AF_USART1);
+      SCU_Periph.SFSP7 (2).EPD := Disable_Pull_Down;
+      SCU_Periph.SFSP7 (2).EPUN := Disable_Pull_Up;
+      SCU_Periph.SFSP7 (2).EZI := Enable_Input_Buffer;
+      SCU_Periph.SFSP7 (2).ZIF := Disable_Input_Glitch;
+      SCU_Periph.SFSP7 (2).MODE := Function_6;
 
-      USART1_Periph.BRR :=
-        (DIV_Fraction => UInt4  (((Frac_Divider * 16 + 50) / 100) mod 16),
-         DIV_Mantissa => UInt12 (Int_Divider / 100),
-         others => <>);
-      USART1_Periph.CR1 :=
-        (UE => 1,
-         RE => 1,
-         TE => 1,
-         others => <>);
-      USART1_Periph.CR2 := (others => <>);
-      USART1_Periph.CR3 := (others => <>);
+      CCU1_Periph.CLK_M4_USART2_CFG.RUN := Enabled;
+      CCU1_Periph.CLK_M4_USART2_CFG.AUTO := Enabled;
+      CCU1_Periph.CLK_M4_USART2_CFG.WAKEUP := Enabled;
+
+      CCU2_Periph.CLK_APB2_USART2_CFG.RUN := Enabled;
+      CCU2_Periph.CLK_APB2_USART2_CFG.AUTO := Enabled;
+      CCU2_Periph.CLK_APB2_USART2_CFG.WAKEUP := Enabled;
+
+      USART2_Periph.FCR.FIFOEN := Enabled;
+      USART2_Periph.FCR.TXFIFORES := Clear;
+      USART2_Periph.FCR.RXFIFORES := Clear;
+
+      USART2_Periph.TER.TXEN := 0;
+
+      USART2_Periph.IER.RBRIE := Disable;
+      USART2_Periph.IER.THREIE := Disable;
+      USART2_Periph.IER.RXIE := Disable;
+      USART2_Periph.IER.ABEOINTEN := Disable;
+      USART2_Periph.IER.ABTOINTEN := Disable;
+
+      USART2_Periph.LCR.WLS := LCR_ENUM_8_Bit_Character_Leng;
+      USART2_Periph.LCR.SBS := LCR_ENUM_1_Stop_Bit;
+      USART2_Periph.LCR.PE := Disable_Parity_Gener;
+      USART2_Periph.LCR.PS := Odd_Parity;
+      USART2_Periph.LCR.BC := Disabled;
+      USART2_Periph.LCR.DLAB := Disabled;
+
+      USART2_Periph.ACR.START := Stop;
+      USART2_Periph.ACR.MODE := Mode_0;
+      USART2_Periph.ACR.AUTORESTART := No_Restart;
+      USART2_Periph.ACR.ABEOINTCLR := No_Effect;
+      USART2_Periph.ACR.ABTOINTCLR := No_Effect;
+
+      USART2_Periph.RS485CTRL.NMMEN := Disabled;
+      USART2_Periph.RS485CTRL.RXDIS := Enabled;
+      USART2_Periph.RS485CTRL.AADEN := Disabled;
+      USART2_Periph.RS485CTRL.DCTRL := Disabled;
+      USART2_Periph.RS485CTRL.OINV := Low;
+
+      USART2_Periph.RS485DLY.DLY := 0;
+
+      USART2_Periph.RS485ADRMATCH.ADRMATCH := 0;
+
+      USART2_Periph.LCR.DLAB := Enabled;
+      USART2_Periph.DLL.DLLSB := 83;
+      USART2_Periph.DLM.DLMSB := 0;
+      USART2_Periph.LCR.DLAB := Disabled;
+
+      USART2_Periph.FDR.MULVAL := 5;
+      USART2_Periph.FDR.DIVADDVAL := 15;
+
+      USART2_Periph.TER.TXEN := 1;
+
    end Initialize;
 
    -----------------
@@ -81,20 +118,20 @@ package body System.Text_IO is
    -----------------
 
    function Is_Tx_Ready return Boolean is
-     (USART1_Periph.SR.TC = 1);
+     (USART2_Periph.LSR.THRE = Empty);
 
    -----------------
    -- Is_Rx_Ready --
    -----------------
 
    function Is_Rx_Ready return Boolean is
-     (USART1_Periph.SR.RXNE = 1);
+     (USART2_Periph.LSR.RDR = Data);
 
    ---------
    -- Get --
    ---------
 
-   function Get return Character is (Character'Val (USART1_Periph.DR.DR));
+   function Get return Character is (Character'Val (USART2_Periph.RBR.RBR));
 
    ---------
    -- Put --
@@ -102,7 +139,7 @@ package body System.Text_IO is
 
    procedure Put (C : Character) is
    begin
-      USART1_Periph.DR.DR := Character'Pos (C);
+      USART2_Periph.THR.THR := Character'Pos (C);
    end Put;
 
    ----------------------------
